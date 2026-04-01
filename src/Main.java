5e2926d0abea684ad44ab753559e79da23d30604
ChelsyGail <chelsygail.pena@gmail.com>
2026-04-01 14:31:15 +0200

    Merge branch 'main' into initial-development

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 import members.SystemManager;
-import products.Book;
-import products.Print;
-import products.Template;
-
-import static members.SystemManager.products;
+import static members.SystemManager.myBooks;
 
 public static void main(String[] args) {
-    // all just tests for each product
-    SystemManager.addProduct(new Book(12.3, "Some", "Fiction", "David F.", "unknown", 231));
-    SystemManager.addProduct(new Print(5.99, "Floral", "Romantic", "Dark.", "unknown", 2.4, 3.4));
-    SystemManager.addProduct(new Template(3.99, "Minimal Brown", "Simple", "unknown.", "unknown", "Powerpoint"));
+
 
 
 }
