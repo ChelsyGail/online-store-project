0839688f94d1057b461a672b70fdd79e63a6b817
ChelsyGail <chelsygail.pena@gmail.com>
2026-04-01 14:26:38 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import products.Template;
 import static members.SystemManager.products;
 
 public static void main(String[] args) {
+    // all just tests for each product
     SystemManager.addProduct(new Book(12.3, "Some", "Fiction", "David F.", "unknown", 231));
     SystemManager.addProduct(new Print(5.99, "Floral", "Romantic", "Dark.", "unknown", 2.4, 3.4));
     SystemManager.addProduct(new Template(3.99, "Minimal Brown", "Simple", "unknown.", "unknown", "Powerpoint"));
